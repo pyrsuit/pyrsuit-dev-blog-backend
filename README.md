3701688d1094333d20e2a9dba4cb779321fd7456
zsofiak96 <kalosi.zsofia@gmail.com>
2025-09-13 10:19:27 +0200

    chore: Move django secret to environment

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pyrsuit-dev-blog-backend
 Backend to the pursuit.dev written in Django
+
+## Management
+
+Generate a new `SECRET_KEY`:
+```bash
+python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"
+```
--- pyrsuit/settings.py
+++ pyrsuit/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e0-*09-1)ah#4pc!27f311%hn3vl-q@v+2ayt#0$kmz^yi*4p)"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
